d2c9bf50e9c3e63c642ba78cbeea0dcdb095ee8e
Shun Kong Cheung <shcheung@microsoft.com>
2024-03-30 22:10:49 -0600

    feat(getTagsFromToken): all types at once

--- src/getTagsFromToken.ts
+++ src/getTagsFromToken.ts
@@ -2,21 +2,30 @@ import { Token } from "marked";
 import { ITag, TagType } from "./types";
 import { getIsLink, getIsList, getUniqueTags } from "./utils";
 
-const getTagsByType = (token: Token, tagType: TagType): ITag[] => {
+const getTagType = (text: string) => {
+	const tagTypes = Object.values(TagType);
+	for(let idx = 0; idx < tagTypes.length; idx ++) {
+		const targetTagType = tagTypes[idx];
+		const isTargetTagType = text.startsWith(targetTagType);
+		if(isTargetTagType)  return targetTagType;
+	}
+	return null;
+}
+
+const getAllTags = (token: Token): ITag[] => {
 	const isLink = getIsLink(token);
 	if(!isLink) {
 		return "tokens" in token 
-		? token.tokens.reduce((prev, childToken) => [...prev, ...getTagsByType(childToken, tagType)], [] as ITag[])
+		? token.tokens.reduce((prev, childToken) => [...prev, ...getAllTags(childToken)], [] as ITag[])
 		: getIsList(token)
-		? token.items.reduce((prev, childItem) => [...prev, ...getTagsByType(childItem, tagType)], [] as ITag[])
+		? token.items.reduce((prev, childItem) => [...prev, ...getAllTags(childItem)], [] as ITag[])
 		: [];
 	}
 
-	const isTag = token.text.startsWith(tagType);
-	return isTag ? [{ type: tagType, text: token.text  }] : [];
+	const tagText = token.text
+	const tagType = getTagType(tagText);
+
+	return tagType ? [{ count: 1, type: tagType, text: token.text  }] : [];
 }
 
-
-export const getTagsFromToken = (token: Token):ITag[]  => getUniqueTags(
-	Object.values(TagType).reduce((prev, tagType) => [...prev, ...getTagsByType(token, tagType)], [] as ITag[])
-);
+export const getTagsFromToken = (token: Token):ITag[]  => getUniqueTags(getAllTags(token));
