2a3ed51bc9625744936ec5a597da0aab58e904dd
Shun Kong Cheung <shcheung@microsoft.com>
2024-03-30 22:09:51 -0600

    feat(getUniqueTags): count handling

--- src/utils.ts
+++ src/utils.ts
@@ -11,8 +11,9 @@ export const getUniqueTags = (tags: ITag[]) => {
 	const getIsSameTag = (left: ITag, right:ITag) => left.text === right.text;
 
 	tags.forEach(tag => {
-		const isExist = uniqueTags.find(uniqueTag => getIsSameTag(tag, uniqueTag));
-		if(!isExist) uniqueTags.push(tag);
+		const existingTag = uniqueTags.find(uniqueTag => getIsSameTag(tag, uniqueTag));
+		if(!existingTag) uniqueTags.push({... tag });
+		else  existingTag.count += tag.count;
 	});
 
 	return uniqueTags;
